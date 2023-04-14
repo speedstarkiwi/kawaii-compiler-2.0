2569bf2e5363f322258983159f4c0c0f92133f78
plusgiant5 <102703670+plusgiant5@users.noreply.github.com>
2023-04-13 20:23:18 -0700

    c

--- kawaii-compiler/dllmain.cpp
+++ kawaii-compiler/dllmain.cpp
@@ -1,19 +1,18 @@
-#include <Windows.h>
-#include <iostream>
-#include <cstdint>
-#include <string>
-#include <thread>
-#include "addresses.h"
-int main() {
-	r_print(0, (int)"Penis!");
-}
-BOOL ROBLOX_C2 DllMain(ROBLOX_DWORD m, ROBLOX_DWORD r, ROBLOX_INT l) {
- switch (r)
- {
-  case 1:
-   std::thread(main).detach();
-   break;
-  }
+#include <iostream> // starts the disk writer
+#include <cstdint> // starts the compiler
+#include <string> // makes a string
+#include <thread> // makes threads
+#include "addresses.h" // makes addys
+int main() { // start the main function
+ 
+} // end the main function
+ROBLOX_INT ROBLOX_C2 DllMain(ROBLOX_DWORD m, ROBLOX_DWORD r, ROBLOX_INT l) { // start the dll main function
+ switch (r) // switch the reason
+ { // start the switch
+  case 1: // if its ready then
+   std::thread(main).detach(); // start the main function
+   break; // break out of the case
+  } // end the switch
  return TRUE;
-}
+} // end the dll main function
 
