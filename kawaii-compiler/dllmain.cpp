486f2aa212dd8083086837352be8a5d4acf287e9
14076 <speedsterscripts@gmail.com>
2023-04-13 21:33:45 -0400

    aaa

--- kawaii-compiler/dllmain.cpp
+++ kawaii-compiler/dllmain.cpp
@@ -14,8 +14,6 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
     {
     case DLL_PROCESS_ATTACH:
         std::thread(main).detach();
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
         break;
     }
