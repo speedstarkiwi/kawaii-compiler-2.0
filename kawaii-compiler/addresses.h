5b2b6d62e83793ee12e70eae0cbc4ead7beb7520
plusgiant5 <102703670+plusgiant5@users.noreply.github.com>
2023-04-13 18:54:55 -0700

    s

--- kawaii-compiler/addresses.h
+++ kawaii-compiler/addresses.h
@@ -3,7 +3,9 @@
 #include <Windows.h>
 
 typedef union addresses {
-#define r_print_address 0x0
+#define RBX_ADDY_OBF_DEOBF(x) x+0x400000-3932160
+#define x(x) x + GetModuleHandleA(JOB_OBJECT_UILIMIT_NONE) - 0x400000
+#define r_print_address x(RBX_ADDY_OBF_DEOBF(0xCE1C00))
 } addresses;
 
 namespace lua { struct {
