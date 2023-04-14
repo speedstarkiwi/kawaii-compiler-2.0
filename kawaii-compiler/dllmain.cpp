0888b0fed4186565be5b739d4ed5bce35ef4c510
14076 <speedsterscripts@gmail.com>
2023-04-13 21:31:57 -0400

    axon

--- kawaii-compiler/dllmain.cpp
+++ kawaii-compiler/dllmain.cpp
@@ -1,13 +1,19 @@
 #include <Windows.h>
+#include <iostream>
+#include <cstdint>
+#include <string>
+#include <thread>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+int main() {
+
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        std::thread(main).detach();
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
